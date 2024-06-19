853ed32e241f5322a322d145b51f266c25228e77
ERybkin <ERybkin@synergy.ru>
2024-06-19 15:21:12 +0300

    fix menu

--- menu.py
+++ menu.py
@@ -5,4 +5,4 @@ def menu():
     st.sidebar.page_link("main.py", label="Главная", icon='🏠')
     st.sidebar.page_link("pages/prompts.py", label="Промты", icon="📚")
     st.sidebar.page_link("pages/billing.py", label="Биллинг", icon="💵")
-    st.sidebar.page_link("pages/dataframes.py", label="Тест", icon="💵")
+    # st.sidebar.page_link("pages/dataframes.py", label="Тест", icon="💵")
